85fa261a61b46f0240cf80236ad98cda4f68c2fc
Flávio Santos <flavioraph@live.com>
2021-09-02 10:35:04 -0300

    atualizacao

--- reactAtvidade3/numeros/src/App.js
+++ reactAtvidade3/numeros/src/App.js
@@ -6,13 +6,13 @@ import { useState } from 'react';
 function App() {
   const [number, setNumber] = useState(0)
   const increment = (value) =>{
-    setNumber(number + value)
+    setNumber(value)
   }
   return (
     <div className="App">
       <header className="App-header">
         <span> {number} </span>
-        <button onClick={()=>increment(parseInt(Math.random()*10))}>ADICIONAR</button>
+        <button onClick={()=>increment(parseInt(Math.random()*100 + 1))}>ADICIONAR</button>
       </header>
     </div>
   );
